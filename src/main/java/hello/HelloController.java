639d92e829f0b868a34a4925ec33983888a63054
zonglin li <zonglin_li@umain.ucsb.edu>
2020-01-21 00:33:43 -0800

    edited HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,9 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        String html = "<h1>Hello World!</h1>\n" + "<p>This web app is powered by \n" +
+        "<a href='https://github.com/ucsb-cs56-w20/lab02-zonglinli1'>this github repo</a></p>\n";
+        return html;
     }
     
 }
